d60ff31d2b6276a549f38550cebdf0ae59062b8e
Maximilian Gorn <maximiliangornbw@web.de>
2024-05-16 22:27:09 +0200

    fixRoouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
 
+
+
+const Start = React.lazy(() => import('./Pages/Start'));
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Start} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
