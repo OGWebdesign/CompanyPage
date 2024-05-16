ed2b9a7f2e367dcb78bd333106bfb2dd2a41d38f
Maximilian Gorn <maximiliangornbw@web.de>
2024-05-16 21:21:15 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-
-
-
 function App() {
-
-
   return (
     <>
     <div className="flex justify-center items-center w-full h-screen font-bold text-[#e9e9e9] text-[5rem] bg-[#202020]">OG Webdesign</div>
     </>
   )
 }
-
 export default App
