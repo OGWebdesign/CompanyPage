96db2eb56e8670aafe99ff03e0faac7d80dcaeba
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-03 21:41:25 +0200

    wip

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { HomeIcon } from "../Icons"
 
 export const Navbar = () => {
     // state for animation
@@ -8,6 +9,7 @@ export const Navbar = () => {
     const visibleStyle = "h-[20rem]"
     // tailwind css for notvisible style sheet
     const notVisibleStyle = "h-0"
+    const rotation = visible ? "":"rotate-90"
 
     // start component build
     return (
@@ -28,7 +30,7 @@ export const Navbar = () => {
 
 
                 {/* Navbar Item Layout */}
-                <div className="mobile:w-[7rem] tablet:w-[30rem] desktop:w-[40rem] flex justify-between items-center">
+                <div className="mobile:w-[7rem] tablet:w-[40rem] desktop:w-[40rem] flex justify-between items-center">
                     
                     {/* Navbar Item I */}
                     <a className="flex justify-center items-center mobile:w-[0rem] tablet:w-[8rem] h-[2rem] mobile:mx-0.8 tablet:mx-2 font-mono tablet:font-semibold mobile:text-[0rem] tablet:text-[0rem] desktop:text-[1.2rem]
@@ -43,8 +45,8 @@ export const Navbar = () => {
                      mobile:opacity-0 desktop:opacity-100 text-[#747474] border-[#202020] hover:text-white hover:border-2 hover:border-b-[#98f144] rounded-md duration-300" href="/">Offer</a>
 
                     {/* Burger Menu Button */}
-                    <div onClick={()=>setVisible(!visible)} className="w-16 h-16 rounded-xl duration-300 mobile:opacity-100 tablet:opacity-100 desktop:opacity-0 flex justify-center items-center hover:cursor-pointer ">
-                        <img className="w-8 h-8 tablet:w-8 tablet:h-8" src="src/assets/house-solid.svg" alt="Menu Icon. Push for scroll menu scroll out" />
+                    <div onClick={()=>setVisible(!visible)} className="w-20 h-16 rounded-xl duration-300 mobile:opacity-100 tablet:opacity-100 desktop:opacity-0 flex justify-center items-center hover:cursor-pointer ">
+                      <HomeIcon className={`w-12 h-12 fill-slate-300 ${rotation} duration-300  hover:fill-[#98f144] flex`}></HomeIcon>
                     </div>
                 </div>
             </div>
@@ -53,13 +55,13 @@ export const Navbar = () => {
             {/* Croll Bar Wrapper */}
             <div className={`w-full mobile:opacity-100 tablet:opacity-100 desktop:opacity-0 desktop:h-0 flex flex-col justify-center items-center ${visible ? visibleStyle:notVisibleStyle} duration-300 bg-[#202020]`}>
                  {/* Croll Bar Item I */}
-                <a className="my-5 mr-3 text-[#747474]  font-mono font-bold text-[1.2rem] hover:text-[#98f144] overflow-hidden"
+                <a className="my-5 mr-3 text-[#747474]  font-mono font-bold text-[1.2rem] hover:text-[#98f144] overflow-hidden hover:underline"
                  href="/"> Start</a>
                  {/* Croll Bar Item II */}
-                <a className="my-5 mr-3 text-[#747474]  font-mono font-bold text-[1.2rem] hover:text-[#98f144] overflow-hidden"
+                <a className="my-5 mr-3 text-[#747474]  font-mono font-bold text-[1.2rem] hover:text-[#98f144] overflow-hidden hover:underline"
                  href="/">Company</a>
                  {/* Croll Bar Item III */}
-                <a className="my-5 mr-3 text-[#747474]  font-mono font-bold text-[1.2rem] hover:text-[#98f144] overflow-hidden"
+                <a className="my-5 mr-3 text-[#747474]  font-mono font-bold text-[1.2rem] hover:text-[#98f144] hover:underline overflow-hidden"
                  href="/">Offer</a> 
             </div>
         </div> // End
