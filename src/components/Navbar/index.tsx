dc825d0ef126842b32a8551e20c430dca049d852
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-03 21:58:41 +0200

    wip

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
     const visibleStyle = "h-[20rem]"
     // tailwind css for notvisible style sheet
     const notVisibleStyle = "h-0"
-    const rotation = visible ? "":"rotate-90"
+    const rotation = visible ? "fill-[#98f144]" : "rotate-90"
 
     // start component build
     return (
@@ -19,7 +19,7 @@ export const Navbar = () => {
             {/* Navbar Item Wrapper */}
             <div className="w-full flex justify-between duration-300">
 
-               {/* Navbar Logo Wrapper */}
+                {/* Navbar Logo Wrapper */}
                 <div className="tablet:w-1/2 duration-300">
 
                     {/* Navbar Logo */}
@@ -31,38 +31,43 @@ export const Navbar = () => {
 
                 {/* Navbar Item Layout */}
                 <div className="mobile:w-[7rem] tablet:w-[40rem] desktop:w-[40rem] flex justify-between items-center">
-                    
+
                     {/* Navbar Item I */}
                     <a className="flex justify-center items-center mobile:w-[0rem] tablet:w-[8rem] h-[2rem] mobile:mx-0.8 tablet:mx-2 font-mono tablet:font-semibold mobile:text-[0rem] tablet:text-[0rem] desktop:text-[1.2rem]
                     mobile:opacity-0 desktop:opacity-100 text-[#747474] border-[#202020] hover:text-white hover:border-2 hover:border-b-[#98f144] rounded-md duration-300" href="/">Start</a>
-                    
+
                     {/* Navbar Item II */}
                     <a className="flex justify-center items-center mobile:w-[0rem] tablet:w-[8rem] h-[2rem] mobile:mx-0.8 tablet:mx-2 font-mono tablet:font-semibold mobile:text-[0rem] tablet:text-[0rem] desktop:text-[1.2rem]
                     mobile:opacity-0 desktop:opacity-100 text-[#747474] border-[#202020] hover:text-white hover:border-2 hover:border-b-[#98f144] rounded-md duration-300" href="/">Company</a>
-                    
+
                     {/* Navbar Item III */}
                     <a className="flex justify-center items-center mobile:w-[0rem] tablet:w-[8rem] h-[2rem] mobile:mx-0.8 tablet:mx-2 font-mono tablet:font-semibold mobile:text-[0rem] tablet:text-[0rem] desktop:text-[1.2rem]
                      mobile:opacity-0 desktop:opacity-100 text-[#747474] border-[#202020] hover:text-white hover:border-2 hover:border-b-[#98f144] rounded-md duration-300" href="/">Offer</a>
 
                     {/* Burger Menu Button */}
-                    <div onClick={()=>setVisible(!visible)} className="w-20 h-16 rounded-xl duration-300 mobile:opacity-100 tablet:opacity-100 desktop:opacity-0 flex justify-center items-center hover:cursor-pointer ">
-                      <HomeIcon className={`w-12 h-12 fill-slate-300 ${rotation} duration-300  hover:fill-[#98f144] flex`}></HomeIcon>
+                    <div onClick={() => setVisible(!visible)} className="w-20 h-16 rounded-xl duration-300 mobile:opacity-100 tablet:opacity-100 desktop:opacity-0 flex justify-center items-center hover:cursor-pointer ">
+                        <HomeIcon className={`w-12 h-12 fill-slate-300 ${rotation} duration-300  hover:fill-[#98f144] flex`}></HomeIcon>
                     </div>
                 </div>
             </div>
 
 
             {/* Croll Bar Wrapper */}
-            <div className={`w-full mobile:opacity-100 tablet:opacity-100 desktop:opacity-0 desktop:h-0 flex flex-col justify-center items-center ${visible ? visibleStyle:notVisibleStyle} duration-300 bg-[#202020]`}>
-                 {/* Croll Bar Item I */}
+            <div className={`w-full mobile:opacity-100 tablet:opacity-100 desktop:opacity-0 desktop:h-0 flex flex-col justify-center items-center ${visible ? visibleStyle : notVisibleStyle} duration-300 bg-[#202020]`}>
+                {/* Croll Bar Item I */}
                 <a className="my-5 mr-3 text-[#747474]  font-mono font-bold text-[1.2rem] hover:text-[#98f144] overflow-hidden"
-                 href="/"> Start</a>
-                 {/* Croll Bar Item II */}
+                    href="/">START</a>
+                {/* Croll Bar Item II */}
                 <a className="my-5 mr-3 text-[#747474]  font-mono font-bold text-[1.2rem] hover:text-[#98f144] overflow-hidden"
-                 href="/">Company</a>
-                 {/* Croll Bar Item III */}
+                    href="/">COMPANY</a>
+                {/* Croll Bar Item III */}
                 <a className="my-5 mr-3 text-[#747474]  font-mono font-bold text-[1.2rem] hover:text-[#98f144] overflow-hidden"
-                 href="/">Offer</a> 
+                    href="/">OFFER</a>
+                <div className="w-full h-5  flex justify-center mt-10 items-center text-[#858585] overflow-hidden font-mono">
+                    <a className="p-3 mx-3 hover:text-[#98f144]" href="">Impressum</a>
+                    <a className="p-3 mx-3 hover:text-[#98f144]" href="">Datenschutzerklärung</a>
+                    <a className="p-3 mx-3 hover:text-[#98f144]" href="">Kontakt</a>
+                </div>
             </div>
         </div> // End
     )
