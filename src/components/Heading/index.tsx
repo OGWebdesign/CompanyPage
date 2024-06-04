1fb1918f83f69897a7f3c7a180f60b38c6375edf
ottonils <ottonils@b-tu.de>
2024-06-04 13:21:08 +0200

    wip

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -6,9 +6,9 @@ export const Heading = (props: HeadingProps) => {
   return (
     <div className="w-full px-8 pt-8">
       <h1
-        className="inline mobile:w-full bg-[#98f144] tablet:w-[90%] mobile:text-[1.7rem] 
+        className="mobile:w-full tablet:w-[90%] mobile:text-[1.7rem] 
       tablet:text-[2rem] desktop:w-[43%] desktop-l:w-[] desktop-xl:w-[] desktop-xxl:w-[]
-      font-bold text-center"
+      font-bold inline "
       >
         {props.heading}
       </h1>
