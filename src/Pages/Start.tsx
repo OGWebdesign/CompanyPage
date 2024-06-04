4f568dcbef46bcd61991b1e4ffc566a4dc2f76e5
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-05 01:10:28 +0200

    fix

--- src/Pages/Start.tsx
+++ src/Pages/Start.tsx
@@ -34,9 +34,9 @@ function App() {
 
       {/* WrapperLayout */}
       <div className="w-full grid gap-11 mobile:gap-20  mobile:grid-rows-4 mobile:grid-cols-1 tablet:grid-cols-1 tablet:grid-rows-2 desktop:grid-rows-1 desktop:grid-cols-2 place-content-center place-items-center mt-20 mb-[10%]">
-        <CardBlack classname="tablet:-translate-x-20" text1="Search enginge" text2="optimization" discription="A huge part of users ignore the second page of search results. Let's make your site visible and drive traffic."></CardBlack>
-        <CardWhite classname="tablet:translate-x-20" text1="Webdesign &" text2="programming" description="50% of website visitors leave after a loading time of 3 seconds. Be the owner of a website that finishes loading in 200ms."></CardWhite>
-        <CardWhite classname="tablet:-translate-x-20" text1="Consulting &" text2="administration" description="Even if you need advice on your content management system, we advise you or administer your existing website."></CardWhite>
+        <CardBlack classname="tablet:-translate-x-20" text1="Search enginge" text2="optimization" discription="A huge part of users ignore lower entries of search results. Let's make your site visible and drive traffic."></CardBlack>
+        <CardWhite classname="tablet:translate-x-20" text1="Webdesign &" text2="programming" description="50% of website visitors leave after a loading time of 3 seconds. We build website that finishes loading in 200ms."></CardWhite>
+        <CardWhite classname="tablet:-translate-x-20" text1="Consulting &" text2="administration" description="If you need advice on your content management system, we advise or administer your existing website."></CardWhite>
         <CardBlack classname="tablet:translate-x-20" text1="Artificial" text2="intelligence" discription="Created images and text by a powerful AI, perfect for clients with limited budgets."></CardBlack>
       </div>
       <LargeCard></LargeCard>
