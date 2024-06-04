222892a0aa338d610b0e7d1798cd596989da56bd
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-04 23:30:58 +0200

    fixx

--- src/components/Allert/index.tsx
+++ src/components/Allert/index.tsx
@@ -1,6 +1,6 @@
 export const Allert = () => {
     return (
-        <div className="w-[100%] h-[5rem] mobile:my-[30%] my-10 flex justify-center items-center">
+        <div className="w-[100%] h-[5rem] mobile:my-[30%] desktop:my-[15%] my-10 flex justify-center items-center">
             <div className="w-[10rem] flex flex-row items-center">
                 <span className=" w-[10rem] font-mono text-[#151515] font-bold text-[1.8rem] bg-[#98f144] rounded pl-2 flex justify-center items-center">Services</span>
             </div>
