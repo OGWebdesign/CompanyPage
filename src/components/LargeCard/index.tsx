4046cbf04491e60cc7b9ad84ee7607c4ced0731e
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-04 23:16:42 +0200

    fix

--- src/components/LargeCard/index.tsx
+++ src/components/LargeCard/index.tsx
@@ -6,7 +6,7 @@ export const LargeCard = () =>{
                     <div className="w-full h-1/2">
                         <p className="pl-5 pt-5 text-[1.7rem] text-[#151515] font-mono font-semibold">Let`s make things happen</p>
                         <p className="pl-5 pt-2 font-mono text-[1.04rem]">Contact us today to learn more about how out digital marketing service can help your business grow and succeed online.</p>
-                        <div className="w-[16rem] mb-4 h-[3.1rem] font-mono text-[1rem] text-slate-200 rounded-xl ml-5 mt-5 bg-[#151515] border-black border font-semibold duration-300 flex justify-center items-center hover:border-[#151515] hover:text-[#151515] hover:cursor-pointer hover:bg-[#98f144]">
+                        <div className="w-[16rem] mb-4 h-[3.1rem] font-mono text-[1rem] text-[#151515] hover:text-slate-200 rounded-xl ml-5 mt-5 bg-[#98f144] border-black font-semibold duration-300 flex justify-center items-center hover:cursor-pointer hover:bg-[#151515]">
                             Get your free proposal
                         </div>
                     </div>
