28a46629043935a2f29b09991dc4dcc0f2b3ae05
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-04 23:22:49 +0200

    rfix

--- src/components/Allert/index.tsx
+++ src/components/Allert/index.tsx
@@ -5,7 +5,7 @@ export const Allert = () => {
                 <span className=" w-[10rem] font-mono text-[#151515] font-bold text-[1.8rem] bg-[#98f144] rounded pl-2 flex justify-center items-center">Services</span>
             </div>
             <div className="w-2/3">
-                <p className="ml-10 font-mono  text-[#777777] text-[1rem]">At our digital merketing agency, we offer a range of services to help businesses grow and succeed online. These services inlude:</p>
+                <p className="ml-10 font-mono  text-[#777777] text-[1rem]">At our digital marketing agency, we offer a range of services to help businesses grow and succeed online. These services inlude:</p>
             </div>
         </div>
     )
