c1f70e19aca5c6b9d09e99c46d5bc814a8810c6e
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-07 00:30:46 +0200

    fix

--- src/components/EmployeeCard/index.tsx
+++ src/components/EmployeeCard/index.tsx
@@ -1,22 +1,22 @@
-export const EmployeeCard = ()=>{
-    return(
-        <div className="w-full flex justify-center items-center bg-[#151515] gap-7 mt-[5%] border-white">
+export const EmployeeCard = () => {
+    return (
+        <div className="w-full flex justify-center items-center bg-[#151515] gap-20 mt-[25%] border-white">
 
             {/* Left Side */}
             <div className="w-1/2 h-full flex flex-col">
-            <div className="flex justify-end">
-                <div className="w-[8rem] h-[8rem] bg-nils hover:cursor-pointer hover:scale-110 duration-300 hover:border-8 grayscale hover:grayscale-0  hover:border-[#98f144] rounded-full border-slate-200 border-2 bg-no-repeat bg-cover">
-                    
+                <div className="flex justify-end">
+                    <img src="public/max.png" className="w-[10rem] h-[10rem]  hover:cursor-pointer hover:scale-110 duration-300 hover:border-8 grayscale hover:grayscale-0  hover:border-[#98f144] rounded-full border-slate-200 border-2 bg-no-repeat bg-cover">
+
+                    </img>
                 </div>
-            </div>
             </div>
 
 
             {/* Right Side */}
             <div className="w-1/2 h-full flex flex-col">
-            <div className="w-[8rem] h-[8rem] bg-max hover:cursor-pointer hover:scale-110 duration-300 hover:border-8 grayscale hover:grayscale-0  hover:border-[#98f144] rounded-full border-slate-200 border-2 bg-no-repeat bg-cover">
-                    
-                </div>
+                <img src="public/nils.png" className="w-[10rem] h-[10rem] hover:cursor-pointer hover:scale-110 duration-300 hover:border-8 grayscale hover:grayscale-0  hover:border-[#98f144] rounded-full border-slate-200 border-2 bg-no-repeat bg-cover">
+
+                </img>
             </div>
         </div>
     )
