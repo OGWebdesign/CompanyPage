4e60dca5314840dbe2265187f774e99424a72470
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-07 01:32:27 +0200

    fix

--- src/components/LargeCard/index.tsx
+++ src/components/LargeCard/index.tsx
@@ -2,8 +2,8 @@ export const LargeCard = () => {
     return (
         <div className="w-full flex justify-center items-center select-none leading-none desktop:mt-[5%]">
             <div className="flex-col max-w-[60rem] min-h-[15em]  bg-slate-200 rounded-2xl">
-                <div className="w-full flex">
-                    <div className="w-1/2 h-full flex-row  mobile:flex-col">
+                <div className="w-full flex desktop:flex-row mobile:flex-col">
+                    <div className="w-1/2 mobile:w-full flex-row  mobile:flex-col">
                         <div className="w-full h-1/2">
                             <p className="pl-5 pt-5 text-[1.7rem] text-[#151515] font-mono font-semibold">Let`s make things happen!</p>
                             <p className="pl-5 pt-2 font-semibold font-mono text-[1.04rem]">No matter what your idea is, we can be the right partner for you. Send us your request and we will find a suitable solution for each of your concerns.</p>
@@ -14,7 +14,7 @@ export const LargeCard = () => {
                     </div>
 
 
-                    <div className="w-1/2 h-full">
+                    <div className="w-1/2 mobile:w-full ">
                         <p className="pl-5 pt-5 text-[1.7rem] text-[#151515] font-mono font-semibold">Whether big or small.</p>
                         <p className="pl-5 pt-2 font-semibold font-mono text-[1.04rem]">You can entrust us with any project proposal, we will find a solution that will ensure your complete satisfaction. Don't hesitate to come to us with small ideas. The biggest companies in the world started in a garage. Dream big, start even bigger.</p>
                     </div>
