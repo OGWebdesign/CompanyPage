623e2eae789a874a605533c929ac487e7d98a67a
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-07 00:37:11 +0200

    fix

--- src/components/EmployeeCard/index.tsx
+++ src/components/EmployeeCard/index.tsx
@@ -5,19 +5,20 @@ export const EmployeeCard = () => {
             {/* Left Side */}
             <div className="w-1/2 h-full flex flex-col">
                 <div className="flex justify-end">
-                    <img src="public/max.png" className="w-[10rem] h-[10rem]  hover:cursor-pointer hover:scale-110 duration-300 hover:border-8 grayscale hover:grayscale-0  hover:border-[#98f144] rounded-full border-slate-200 border-2 bg-no-repeat bg-cover">
+                    <img alt="" src="/public/max.png" className="w-[10rem] h-[10rem]  hover:cursor-pointer hover:scale-110 duration-300 hover:border-8 grayscale hover:grayscale-0  hover:border-[#98f144] rounded-full border-slate-200 border-2 bg-no-repeat bg-cover">
 
                     </img>
-                </div>
-                <div className="">
+                    <div className="text-slate-200">
                     
+                    </div>
                 </div>
+
             </div>
 
 
             {/* Right Side */}
             <div className="w-1/2 h-full flex flex-col">
-                <img src="public/nils.png" className="w-[10rem] h-[10rem] hover:cursor-pointer hover:scale-110 duration-300 hover:border-8 grayscale hover:grayscale-0  hover:border-[#98f144] rounded-full border-slate-200 border-2 bg-no-repeat bg-cover">
+                <img alt="" src="/public/nils.png" className="w-[10rem] h-[10rem] hover:cursor-pointer hover:scale-110 duration-300 hover:border-8 grayscale hover:grayscale-0  hover:border-[#98f144] rounded-full border-slate-200 border-2 bg-no-repeat bg-cover">
 
                 </img>
             </div>
