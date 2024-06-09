1287525a635d45c96fe3657f6e41be7d7aea63b3
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-09 17:00:34 +0200

    wip

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -18,8 +18,8 @@ function App() {
   // state for animation
   const [visible, setVisible] = useState(false) // state for Nav Bar scroll out effect
   const [indicatorNav, setIndicatorNav] = useState(1)
- 
- 
+
+
   // tailwind css for visible style sheet
   const visibleStyle = "h-[20rem]"
   // tailwind css for notvisible style sheet
@@ -39,12 +39,13 @@ function App() {
 
 
   // Button for order meeting for component TextAndButton for working with one Page technologie
-  const orderMeetingbt = (text: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined) => 
-  <div onClick={()=>setIndicatorNav(4)}
-    className="text-[#151515] cursor-none select-none border-none bg-[#98f144] font-semibold mt-8 py-2 px-4 rounded-lg hover:bg-[#6ba039] font-mono transition duration-300 ease-in-out">
-    {text}
-  </div>
+  const orderMeetingbt = (text: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined) =>
+    <div onClick={() => setIndicatorNav(4)}
+      className="text-[#151515] cursor-none select-none border-none bg-[#98f144] font-semibold mt-8 py-2 px-4 rounded-lg hover:bg-[#6ba039] font-mono transition duration-300 ease-in-out">
+      {text}
+    </div>
 
+  const maintainbt = () => <div onClick={() => setIndicatorNav(1)} className="w-[15rem] flex justify-center bg-[#98f144] font-semibold text-[#151515] mt-10 rounded-lg bg-[] font-mono text-[1rem] items-center h-[2rem] duration-300 hover:bg-[#6ba039]">Go back to Main Menu</div>
 
 
 
@@ -58,7 +59,9 @@ function App() {
       <Cursor></Cursor>
 
 
-      <Maintenance></Maintenance>
+      {indicatorNav === 5 && (
+        <Maintenance children={maintainbt()}></Maintenance>
+      )}
 
       {/* Wrapper */}
       <div className="w-full bg-[#151515] cursor-none">
@@ -101,7 +104,7 @@ function App() {
                     mobile:opacity-0 mobile:w-0 desktop:opacity-100 border-[#151515] hover:text-white hover:border-b-8 rounded-lg duration-300`} >Company</div>
 
                     {/* Navbar Item III */}
-                    <div className={`${offerActive} mobile:-z-10 cursor-none tablet:-z-10 desktop:z-0 flex justify-center items-center mobile:w-[0rem] tablet:w-[8rem] h-[2rem] mobile:mx-0.8 tablet:mx-2 font-mono tablet:font-semibold mobile:text-[0rem] tablet:text-[0rem] desktop:text-[1.2rem]
+                    <div onClick={() => setIndicatorNav(5)} className={`${offerActive} mobile:-z-10 cursor-none tablet:-z-10 desktop:z-0 flex justify-center items-center mobile:w-[0rem] tablet:w-[8rem] h-[2rem] mobile:mx-0.8 tablet:mx-2 font-mono tablet:font-semibold mobile:text-[0rem] tablet:text-[0rem] desktop:text-[1.2rem]
                      mobile:opacity-0 desktop:opacity-100 border-[#151515] hover:text-white hover:border-b-8 rounded-lg duration-300`} >Offer</div>
 
                     {/* Burger Menu Button */}
@@ -120,17 +123,17 @@ function App() {
                 <div className={`w-full mobile:opacity-100 tablet:opacity-100 desktop:opacity-0 desktop:h-0 flex flex-col justify-center items-center ${visible ? visibleStyle : notVisibleStyle} duration-300 bg-[#151515]`}>
                   {/* Scroll Bar Item I */}
                   <div className={`my-5 ${bordervisibility} text-[#747474] cursor-none w-[10rem] text-center font-mono text-[1.2rem] hover:text-[#98f144] hover:animate-ownpulse border-[#151515] hover:border-[#98f144] rounded-lg overflow-hidden`}
-                    onClick={() => {setIndicatorNav(1); setVisible(!visible);}}>Start</div>
+                    onClick={() => { setIndicatorNav(1); setVisible(!visible); }}>Start</div>
                   {/* Scroll Bar Item II */}
                   <div className={`my-5 ${bordervisibility} text-[#747474] cursor-none w-[10rem] text-center  font-mono text-[1.2rem] hover:text-[#98f144] hover:animate-ownpulse border-[#151515] hover:border-[#98f144] rounded-lg overflow-hidden`}
-                    onClick={() => {setIndicatorNav(2); setVisible(!visible);}}>Company</div>
+                    onClick={() => { setIndicatorNav(2); setVisible(!visible); }}>Company</div>
                   {/* Scroll Bar Item III */}
                   <div className={`my-5 ${bordervisibility}  text-[#747474] cursor-none w-[10rem] text-center font-mono text-[1.2rem] hover:text-[#98f144] hover:animate-ownpulse border-[#151515] hover:border-[#98f144] rounded-lg overflow-hidden`}
-                    onClick={() => {setIndicatorNav(3); setVisible(!visible);}}>Offer</div>
+                    onClick={() => { setIndicatorNav(5); setVisible(!visible); }}>Offer</div>
                   <div className="w-full h-5  flex justify-center text-[0.8rem] mt-10 items-center text-[#858585] overflow-hidden font-mono">
-                    <div className="p-3 mx-3 cursor-none hover:text-[#98f144]" >Imprint</div>
-                    <div className="p-3 mx-3 cursor-none hover:text-[#98f144]" >Privacy</div>
-                    <div onClick={() => {setIndicatorNav(4); setVisible(!visible);}} className="p-3 mx-3 cursor-none hover:text-[#98f144]" >Contact</div>
+                    <div onClick={() => setIndicatorNav(5)} className="p-3 mx-3 cursor-none hover:text-[#98f144]" >Imprint</div>
+                    <div onClick={() => setIndicatorNav(5)} className="p-3 mx-3 cursor-none hover:text-[#98f144]" >Privacy</div>
+                    <div onClick={() => { setIndicatorNav(4); setVisible(!visible); }} className="p-3 mx-3 cursor-none hover:text-[#98f144]" >Contact</div>
                   </div>
                   <div className={`${stroke} duration-500 mt-5  border-2 border-[#98f144] rounded-full mb-5`}></div>
                 </div>
@@ -139,7 +142,7 @@ function App() {
               {/* EndNav */}
               {/* End NavigationBar*/}
 
-{/*---------------------------------------------------------------------- Here come in all new sites as component ------------------------------------ */}
+              {/*---------------------------------------------------------------------- Here come in all new sites as component ------------------------------------ */}
 
               {/* Integration for One Page Technologie */}
               {/* get you component for linkship and add an onCLick event. Now you can render dynamicly with an indicator Number */}
@@ -155,8 +158,7 @@ function App() {
                 <ContactSite></ContactSite>
               )}
 
-               
-{/*------------------------------------------------------------------------ End Dynamic Render Area -------------------------------------------------------*/}
+              {/*------------------------------------------------------------------------ End Dynamic Render Area -------------------------------------------------------*/}
             </div>
           </div>
         </div>
@@ -192,19 +194,19 @@ function App() {
               {/* Last TextFragment */}
               <article className="desktop:text-[6rem] tablet:text-[4rem] mobile:text-[2rem]  desktop:w-[50rem] tablet:w-[35rem] mobile:w-[13rem] inline-block bg-[#98f144] text-[#151515] font-bold leading-none">TALK TO US! </article>
               {/* Button */}
-              <a className="desktop:w-[10rem] cursor-none tablet:w-[8rem] tablet:h-[3rem] desktop:h-[4rem] desktop:text-[1.4rem] mobile:w-[6rem] mobile:h-[2rem] mobile:rounded-lg mobile:ml-[1rem] mobile:text-[1rem] font-mono ml-[2rem] duration-300 border-[#151515] mobile:hover:border-2 desktop:hover:border-4 hover:bg-[#98f144] hover:font-bold hover:text-[#151515] rounded-2xl bg-[#151515] text-center flex justify-center items-center" href="">
+              <div onClick={() => setIndicatorNav(4)} className="desktop:w-[10rem] cursor-none tablet:w-[8rem] tablet:h-[3rem] desktop:h-[4rem] desktop:text-[1.4rem] mobile:w-[6rem] mobile:h-[2rem] mobile:rounded-lg mobile:ml-[1rem] mobile:text-[1rem] font-mono ml-[2rem] duration-300 border-[#151515] mobile:hover:border-2 desktop:hover:border-4 hover:bg-[#98f144] hover:font-bold hover:text-[#151515] rounded-2xl bg-[#151515] text-center flex justify-center items-center" href="">
                 Lets talk
-              </a>
+              </div>
             </div>
 
 
             {/* Footer Nav Bar */}
             <div className="w-full cursor-none flex font-mono font-semibold text-[#151515] mt-[3%] duration-300  items-center tablet:text-[1rem] mobile:text-[0.8rem]">
               <div className="w-1/2 flex desktop:gap-10 tablet:gap-6 mobile:gap-4 justify-start" >
-                <div className="inline-block hover:bg-[#98f144] cursor-none duration-300 hover:font-bold" >Imprint</div>
-                <div className="inline-block hover:bg-[#98f144] cursor-none duration-300 hover:font-bold" >Privacy</div>
+                <div onClick={() => setIndicatorNav(5)} className="inline-block hover:bg-[#98f144] cursor-none duration-300 hover:font-bold" >Imprint</div>
+                <div onClick={() => setIndicatorNav(5)} className="inline-block hover:bg-[#98f144] cursor-none duration-300 hover:font-bold" >Privacy</div>
                 <div onClick={() => setIndicatorNav(4)} className="inline-block hover:bg-[#98f144] cursor-none duration-300 hover:font-bold" >Contact</div>
-                <div className="inline-block hover:bg-[#98f144] cursor-none duration-300 hover:font-bold" >Offer</div>
+                <div onClick={() => setIndicatorNav(5)} className="inline-block hover:bg-[#98f144] cursor-none duration-300 hover:font-bold" >Offer</div>
               </div>
               <div className="w-1/2 flex desktop:gap-8 tablet:gap-6 mobile:gap-4 items-center justify-end">
                 <p className="inline-block bg-[#98f144] mobile:opacity-0 ">Social Media</p>
--- src/components/Maintenance/index.tsx
+++ src/components/Maintenance/index.tsx
@@ -1,11 +1,14 @@
 import { GearIcon } from "../Icons"
 
-export const Maintenance = () => {
+export const Maintenance = (props:{
+    children?: JSX.Element | JSX.Element[] | string,
+}) => {
     return (
         <div className="w-full animate-fadeIn cursor-none flex items-center  justify-center flex-col h-full fixed z-10 bg-[#151515]">
             <div className="flex flex-col mobile:p-10">
-                <p className="text-white font-mono text-[2rem]">Maintenance work in progress in this area</p>
+                <p className="text-white font-mono text-[2rem] inline-block bg-[#]">Maintenance work in progress in this area</p>
                 <p className="text-white w-[25rem] font-mono text-[1rem]">This area of ​​the site is undergoing maintenance and will therefore be unavailable for a while.</p>
+               {props.children}
               <div className="w-full flex justify-center mt-[10%]">
                 <GearIcon className="w-[10rem] fill-slate-200 animate-spin">
 
