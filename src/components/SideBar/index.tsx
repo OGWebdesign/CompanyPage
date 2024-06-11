09bfeb2ef9a7f45d4bc0ea230b8d205b351b1cdc
notto04 <nilsoleotto@gmail.com>
2024-06-11 17:28:02 +0200

    fix

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -17,33 +17,36 @@ export const SideBar = (props:SideBarProps) => {
     const [height, setHeight] = useState(true);
     const [counter, setCounter] = useState(1);
 
-    const activeStyle = "translate-x-0";
-    const inactiveStyle = "translate-x-[85%]";
+    const activeStyle = "translate-x-0 h-[12rem]";
+    const inactiveStyle = "translate-x-[85%] h-[5rem]";
+
+    const activeStyleArrow = "h-[12rem]";
+    const inactiveStyleArrow = "h-[5rem]";
 
     const handleClick = () => {
         setCounter(counter + 1);
         if(counter % 2 === 0){
         setTimeout(() => {
             setHeight(!height);
-        }, 1000);
+        }, 100);
         setClick(!click);
         } else {
         setTimeout(() => {
             setClick(!click);
-        }, 500);
+        }, 100);
         setHeight(!height);
         }
     }
 
     return(
-    <div onClick={handleClick} className={`fixed z-20 w-[20rem] top-1/2 right-0 transform translate-y-[7rem] bg-[#98f144] text-[#151515] p-4 
-        rounded-l-lg flex shadow-lg hover:shadow-2xl transition-all duration-500 ease-in-out ${click && activeStyle} ${!click && inactiveStyle}
-        ${height && "h-[5rem]"}`}>
+    <div className={`fixed z-20 w-[20rem] top-1/2 right-0 transform translate-y-[7rem] bg-[#98f144] text-[#151515] pr-4
+        rounded-l-lg flex justify-start items-center shadow-lg hover:shadow-2xl transition-all duration-500 ease-in-out 
+        ${click && activeStyle} ${!click && inactiveStyle} overflow-hidden`}>
 
-        <div className="w-[1rem] flex justify-center items-center transition-all">
-        {}
-        {click && <RightArrowIcon className="bg-fill-[#151515]" />}
-        {!click && <LeftArrowIcon className="bg-fill-[#151515]" />}
+        <div onClick={handleClick} className={`w-full mr-[2rem] flex justify-center items-center transition-all ${click && activeStyleArrow}
+            ${!click && inactiveStyleArrow} `}>
+        {click && <RightArrowIcon className="bg-fill-[#151515] w-[1rem]" />}
+        {!click && <LeftArrowIcon className="bg-fill-[#151515] w-[1rem]" />}
         </div>
 
         <div className="w-full flex flex-col justify-center items-center gap-x-6 transition-all">
