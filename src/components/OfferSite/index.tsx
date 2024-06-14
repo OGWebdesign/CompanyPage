a256bbde229824c1bc282699648e256b5a741ac4
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-14 16:40:27 +0200

    preSpitt

--- src/components/OfferSite/index.tsx
+++ src/components/OfferSite/index.tsx
@@ -241,24 +241,24 @@ export const OfferSite = () => {
     <>
       {/* Headline and Product Desciption */}
       <div className="animate-fadeIn">
-        <ParagraphOG
+        <ParagraphOGLeft
           classnamehead="text-[#98f144]"
           head="What exactly is a SPA?"
           text="A SPA or Single Page Application is a modern technology that does not program websites as such, but rather as a real web-based program. Sounds crazy and it is. The huge advantages of this technology are that there are no loading times. The program is completely preloaded, so to speak. In addition, after loading once, the program no longer needs an internet connection to be displayed properly. There are also lots of new possibilities in terms of animation. Imagine an online shop where you can browse through the individual products and are not subject to any loading times. Porting it as a permanent mobile app is also child's play after such an implementation. However, a large amount of know-how is required. Technical SEO in particular is a challenge here. Thanks to content management systems such as WordPress, we have stopped leaving work on the web to real experts. If you have a broken car, very few people try to tackle it themselves. This is where we come in. We are experts in this field and have made it our mission to make this technology accessible to every company. The advantages are obvious. Almost no loading times, an exceptional user experience and state-of-the-art technology like Facebook or Instagram."
           animation={false}
-        ></ParagraphOG>
+        ></ParagraphOGLeft>
         <ParagraphOGLeft
           classnamehead="text-[#98f144]"
           head="Websites"
           animation={false}
           text="Of course, we can also do classic websites. Here, too, we use developer tools for professional maintainability and performance with the highest security standards. Especially with content management systems, security gaps can quickly and unnoticed creep in through outdated plugins. From a technical point of view, this cannot happen so easily with our websites. The developer tools are created and tested by the really big players such as Facebook and Instagram. This means that a very high security guarantee is given. The performance cannot even come close to being achieved with content management systems. Leave your website to the experts, where real code is still written."
         ></ParagraphOGLeft>
-        <ParagraphOG
+        <ParagraphOGLeft
           classnamehead="text-[#98f144]"
           head="Consulting & administration."
           text="Of course, the basis of computer science is to pass on knowledge. An entire project stands or falls with advice and planning. Especially in the IT jungle, all the confusing terminology can quickly become overwhelming. We are there for your concerns from the very first minute. From planning to implementation, we are available to answer any questions you may have. Get comprehensive advice for your own idea so that it gets the implementation it deserves. Be it a web application, a website or the integration of artificial intelligence or bots."
           animation={false}
-        ></ParagraphOG>
+        ></ParagraphOGLeft>
         <ParagraphOGLeft
           classnamehead="text-[#98f144]"
           head="AI & Bots"
@@ -303,7 +303,7 @@ export const OfferSite = () => {
           >
 
             <ProgressionBar initProgression={5} />
-            
+
             <SwitchesWebsite
               switchTitle1="modern"
               switchTitle2="elegance"
