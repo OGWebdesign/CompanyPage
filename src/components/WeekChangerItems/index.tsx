171eb20ce2a0ef74aa51b7dd1b0d4a720d5b5225
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-15 17:21:31 +0200

    fix

--- src/components/WeekChangerItems/index.tsx
+++ src/components/WeekChangerItems/index.tsx
@@ -5,7 +5,7 @@ export const WeekChangerItems = (props: {
 }) => {
     return (
         <div onClick={props.onClick} className={` duration-300 p-1 flex border-2 border-[#151515] justify-center items-center  ${props.activecss}`}>
-            <div className="w-[98%] h-[98%] p-5 flex justify-center items-center rounded-lg text-center font-semibold  text-[#151515] bg-[#98f144] font-mono">
+            <div className="w-[98%] h-[98%] p-5 flex justify-center items-center rounded-md text-center font-semibold  text-[#151515] bg-[#98f144] font-mono">
                 {props.text}
             </div>
         </div>
