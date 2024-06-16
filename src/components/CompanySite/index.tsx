45dca3bb5226f852fb85e6437d6f9c5d86586f8e
notto04 <nilsoleotto@gmail.com>
2024-06-16 16:08:28 +0200

    add motivation text

--- src/components/CompanySite/index.tsx
+++ src/components/CompanySite/index.tsx
@@ -1,6 +1,7 @@
 import { EmployeeCard } from "../EmployeeCard"
 import { LargeCard } from "../LargeCard"
 import { ParagraphOG } from "../ParagraphOG"
+import { ParagraphOGLeft } from "../ParagraphOGLeft"
 
 export const CompanySite = ()=>{
     return (
@@ -19,7 +20,20 @@ export const CompanySite = ()=>{
                   head1="Since XX23" 
                   text2="At OG Webdesign, we believe that your website should be more than just a digital business card. It should tell your story, bring your brand to life and delight your customers. Our team consists of talented designers, developers and strategists who work with passion and creativity to turn your ideas into reality. Straight forward!" 
                   text1="We look back on a short but successful past. We have implemented every project, every application, no matter how small with great care and an even greater sense of responsibility, always to the full satisfaction of our clients and beyond. We are thrilled to see how our websites and software products have transformed us into a completely different company, from the way we present ourselves online to how we operate our software products internally."></LargeCard>
-        
+            <ParagraphOGLeft 
+            classnamehead="bg-[#98f144] inline-block text-[#151515]"
+            text="The web offers countless opportunities to present yourself or your company in a contemporary and sensible manner. 
+            Despite this, many websites do not fully realize their potential. 
+            Websites are becoming increasingly important in the digital age and are often a key to success. 
+            We want to help you achieve this success and support you with our technical know-how. 
+            Even if functionality is your primary concern, we are the right partner for you. Web applications are becoming increasingly popular 
+            because they operate independently of operating systems. A web app can quickly be turned into a desktop application. Bots also play a 
+            crucial role on the web. Whether used to close security gaps or analyze website activities, they are universally applicable. 
+            Does it all seem a bit overwhelming? Or do you need expert opinions? We are happy to advise you on the advantages and disadvantages of 
+            various technologies and answer your questions."
+            head="#Motivation"
+            animation={false}
+            />
         </div>
     )
 }
