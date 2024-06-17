85fe275fabe98a646f10759bccf1a3381c4a820f
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-17 22:48:03 +0200

    fix

--- src/components/CompanySite/index.tsx
+++ src/components/CompanySite/index.tsx
@@ -3,26 +3,36 @@ import { LargeCard } from "../LargeCard"
 import { ParagraphOG } from "../ParagraphOG"
 import { ParagraphOGLeft } from "../ParagraphOGLeft"
 
-export const CompanySite = ()=>{
+export const CompanySite = () => {
     return (
         <div className="animate-fadeIn">
-             <ParagraphOG classnamehead="inline-block bg-[#98f144] text-[#151515]" text="Hey, we're Max and Nils the creative duo behind OG Webdesign and we are very pleased to meet you. What is our job? To create cool and innovative digital solutions that not only look good, but also have a lot to offer.
+            <ParagraphOG classnamehead="inline-block bg-[#98f144] text-[#151515]" text="Hey, we're Max and Nils the creative duo behind OG Webdesign and we are very pleased to meet you. What is our job? To create cool and innovative digital solutions that not only look good, but also have a lot to offer.
              With our know-how from studying computer science and design and a lot of experience under our belt, we bring your ideas to life online. Whether it's websites, bots, web apps, application software or AI services, we've got it!
-             Every project begins with a detailed discussion to understand your needs and goals. Based on this, we develop individual concepts that are tailored precisely to your company. Transparent communication and close cooperation are a matter of course for us." 
-                 head="#Transparency" animation={false} classname="text-right justify-end"></ParagraphOG>
-                       <EmployeeCard></EmployeeCard>
-                 <LargeCard 
-                  head1style="text-[#98f144] mt-10"
-                  head2style="text-[#98f144] mt-10"
-                  text1Style="text-[#b9b9b9]"
-                  text2Style="text-[#b9b9b9]"
-                  head2="Young and unconventional" 
-                  head1="Since XX23" 
-                  text2="At OG Webdesign, we believe that your website should be more than just a digital business card. It should tell your story, bring your brand to life and delight your customers. Our team consists of talented designers, developers and strategists who work with passion and creativity to turn your ideas into reality. Straight forward!" 
-                  text1="We look back on a short but successful past. We have implemented every project, every application, no matter how small with great care and an even greater sense of responsibility, always to the full satisfaction of our clients and beyond. We are thrilled to see how our websites and software products have transformed us into a completely different company, from the way we present ourselves online to how we operate our software products internally."></LargeCard>
-            <ParagraphOGLeft 
-            classnamehead="bg-[#98f144] inline-block text-[#151515]"
-            text="The web offers countless opportunities to present yourself or your company in a contemporary and sensible manner. 
+             Every project begins with a detailed discussion to understand your needs and goals. Based on this, we develop individual concepts that are tailored precisely to your company. Transparent communication and close cooperation are a matter of course for us."
+                head="#Transparency" animation={false} classname="text-right justify-end"></ParagraphOG>
+            <EmployeeCard></EmployeeCard>
+            <LargeCard
+                head1style="text-[#98f144] mt-10"
+                head2style="text-[#98f144] mt-10"
+                text1Style="text-[#b9b9b9]"
+                text2Style="text-[#b9b9b9]"
+                head2="Young and unconventional"
+                head1="Since XX23"
+                text2="At OG Webdesign, we believe that your website should be more than just a digital business card. It should tell your story, bring your brand to life and delight your customers. Our team consists of talented designers, developers and strategists who work with passion and creativity to turn your ideas into reality. Straight forward!"
+                text1="We look back on a short but successful past. We have implemented every project, every application, no matter how small with great care and an even greater sense of responsibility, always to the full satisfaction of our clients and beyond. We are thrilled to see how our websites and software products have transformed us into a completely different company, from the way we present ourselves online to how we operate our software products internally."></LargeCard>
+           
+           
+           
+            <LargeCard classname="bg-slate-200 text-[#151515]"
+                head1="Creating new opportunities." 
+                head2="Setting new standards in high-performance websites." 
+                text1="We have very high standards for ourselves, whether it's performance optimization or design. The reason why we decided to focus on web development is simple. We want to put an end to the old-fashioned uniformity. It's time to try new things, to stand out and stand out." 
+                text2="New technologies always offer the appeal of overcoming different challenges. They also usually bring new possibilities for software development. It is time to put aside the old habits and take new paths. The courage to create new user experiences will pay off because being the first always pays off.">
+                </LargeCard>
+
+            <ParagraphOGLeft
+                classnamehead="bg-[#98f144] inline-block text-[#151515] mt-[3rem]"
+                text="The web offers countless opportunities to present yourself or your company in a contemporary and sensible manner. 
             Despite this, many websites do not fully realize their potential. 
             Websites are becoming increasingly important in the digital age and are often a key to success. 
             We want to help you achieve this success and support you with our technical know-how. 
@@ -31,8 +41,8 @@ export const CompanySite = ()=>{
             crucial role on the web. Whether used to close security gaps or analyze website activities, they are universally applicable. 
             Does it all seem a bit overwhelming? Or do you need expert opinions? We are happy to advise you on the advantages and disadvantages of 
             various technologies and answer your questions."
-            head="#Motivation"
-            animation={false}
+                head="#Motivation"
+                animation={false}
             />
         </div>
     )
--- src/components/EmployeeCard/index.tsx
+++ src/components/EmployeeCard/index.tsx
@@ -1,4 +1,5 @@
 
+
 export const EmployeeCard = () => {
     return (
         <div className="w-full flex justify-center items-center">
@@ -8,13 +9,13 @@ export const EmployeeCard = () => {
                 <div className="w-full flex tablet:flex-row group mobile:justify-center mobile:items-center mobile:flex-col-reverse ">
                     <div className="text-slate-200">
                         <p className="font-mono font-bold text-[2rem] text-[#98f144]">Hi, I'm Nils.</p>
-                        Informatics has fascinated me ever since. I always wanted to understand how computers and programs work on them. 
-                        The world of the web, in particular, captivates me. Almost nothing is impossible, and the possibilities are becoming 
-                        increasingly diverse. Max and I met during our studies and decided to start a company together to realize our ideas and visions. 
+                        Informatics has fascinated me ever since. I always wanted to understand how computers and programs work on them.
+                        The world of the web, in particular, captivates me. Almost nothing is impossible, and the possibilities are becoming
+                        increasingly diverse. Max and I met during our studies and decided to start a company together to realize our ideas and visions.
                         Whether it's large or small websites/web apps, consulting, bots, or AI interfaces, we are here for you.
                     </div>
                     <div className="p-8">
-                        
+
                         <img loading="lazy" className="min-w-[5rem] max-w-[15rem] grayscale group-hover:grayscale-0 group-hover:scale-110 duration-300  mobile:rounded-full mobile:mb-10 mobile:border-8 border-[#98f144]" src="nils.webp" alt="" />
                     </div>
                 </div>
@@ -27,14 +28,15 @@ export const EmployeeCard = () => {
                     </div>
                     <div className="text-slate-200">
                         <p className="font-mono font-bold text-[2rem] text-[#98f144]">Hi, I'm Max.</p>
-                        I am a passionate programmer and after my active service as a soldier I decided to study computer science and start my 
-                        own business in web and application development. I have a lot of experience in web applications and frontend development, 
-                        but I am also familiar with bot networks and interfaces for artificial intelligence. Be it React, Typescript, Java or C#. 
+                        I am a passionate programmer and after my active service as a soldier I decided to study computer science and start my
+                        own business in web and application development. I have a lot of experience in web applications and frontend development,
+                        but I am also familiar with bot networks and interfaces for artificial intelligence. Be it React, Typescript, Java or C#.
                         We have extensive experience in various front- and back-end technologies and have programmed everything from small web applications
-                         or websites to cooperative app development projects with the healthcare sector.
+                        or websites to cooperative app development projects with the healthcare sector.
                     </div>
-
+                    
                 </div>
+               
             </div>
         </div>
     )
