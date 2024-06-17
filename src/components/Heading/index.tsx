30ffe413d78b3b38df503953b277fa72a2a3f206
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-17 21:36:11 +0200

    wip

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -7,7 +7,7 @@ type HeadingProps = {
 export const Heading = (props: HeadingProps) => {
 
   return (
-    <div className={`w-full px-8 pt-8 mt-10 flex ${props.classname} `}>
+    <div className={`w-full px-8 pt-8 flex ${props.classname} `}>
       <div className="mobile:w-full tablet:w-[60%] laptop:w-1/2">
         <h1
           className={`font-mono leading-[1.25] ${props.headstyle} select-none mobile:text-[1.7rem] 
