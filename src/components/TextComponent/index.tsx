414e9216528ad4a227636decb87c4a927f11d859
Maximilian Gorn <maximiliangornbw@web.de>
2024-06-17 21:10:18 +0200

    wip

---
+++ src/components/TextComponent/index.tsx
@@ -0,0 +1,11 @@
+export const TextComponent = (props:{
+    headline:string,
+    text:string
+}) => {
+    return (
+        <div className="w-full mt-[3rem] flex flex-col justify-center items-center">
+            <p className="text-[#98f144] w-[80%] leading-9 text-[2rem] font-mono font-semibold">{props.headline}</p>
+            <p className="text-slate-200 w-[80%] leading-6 mt-5  font-mono">{props.text}</p>
+        </div>
+    )
+}
