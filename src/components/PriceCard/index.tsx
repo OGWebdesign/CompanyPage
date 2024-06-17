2f2867ce37d06a01f0e70d3c8d6cb8c5e61b7d42
notto04 <nilsoleotto@gmail.com>
2024-06-17 22:43:27 +0200

    wip

--- src/components/PriceCard/index.tsx
+++ src/components/PriceCard/index.tsx
@@ -36,30 +36,30 @@ export const PriceCard = (props: PriceCardProps) => {
             {/* Start Up Package */}
             case 2 : 
                 {/* 6-8 Wochen */}
-                if (weeksSelected === 1){
+                if (weeksSelected == 1){
                     return StartUpPrice6to8weeks;
                 } 
                 /* 8-10 Wochen */
-                else if (weeksSelected === 2){
+                else if (weeksSelected == 2){
                     return StartUpPrice8to10weeks;
                 }
                 /* 10-12 Wochen */
-                else if (weeksSelected === 3){
+                else if (weeksSelected == 3){
                     return StartUpPrice10to12weeks;
                 }
             break;
             {/* Business Package */}
             case 3 :
                  {/* 6-8 Wochen */}
-                 if (weeksSelected === 1){
+                 if (weeksSelected == 1){
                     return BusinessPrice6to8weeks;
                  } 
                  /* 8-10 Wochen */
-                 else if (weeksSelected === 2){
+                 else if (weeksSelected == 2){
                     return BusinessPrice8to10weeks;
                  }
                  /* 10-12 Wochen */
-                 else if (weeksSelected === 3){
+                 else if (weeksSelected == 3){
                     return BusinessPrice10to12weeks;
                  }
              break;
@@ -139,6 +139,147 @@ export const PriceCard = (props: PriceCardProps) => {
             {/* DESCRIPTION */}
             <p className="text-[0.7rem] w-[15rem] text-center">
                 An imprint brand is a reference to us and it is explicitly mentioned that OG-Webdesign created this site.
+            </p>
+
+            {/* PRICE */}
+            <span className={`text-[#151515] text-[1.6rem] inline-block bg-[#98f144] mt-[1.5rem]`}>{priceDot(currentPrice!)},00€</span>
+            
+
+        </div>
+    );
+}
+
+export const PriceCardWebApp = (props: PriceCardProps) => {
+
+    {/* Preiskonstanten */}
+    {/* ONE PAGER */}
+    const miniAppPrice = 1750;
+    {/* START UP */}
+    const mediumApp6to8weeks = 3700;
+    const mediumApp8to10weeks = 3500;
+    const mediumApp10to12weeks = 3300;
+    {/* BUSINESS */}
+    const LargeApp6to8weeks = 6000;
+    const LargeApp8to10weeks = 5800;
+    const LargeApp10to12weeks = 5600;
+
+    {/* Bestimmung der Preise durch Angabe der Auswahl
+        weeks: 1-(6-8), 2-(8-10), 3-(10-12)
+        package: 1-(OnePager), 2-(Start Up), 3-(Business)
+    */}
+    const price = (weeksSelected : number | undefined, packageSelected : number | undefined) : number | undefined => {
+        switch(packageSelected){
+            /* OnePager */
+            case 1 :
+                {/* 4-6 Wochen ausgewählt */}
+                return miniAppPrice;       
+            break;
+            {/* Start Up Package */}
+            case 2 : 
+                {/* 6-8 Wochen */}
+                if (weeksSelected === 1){
+                    return mediumApp6to8weeks;
+                } 
+                /* 8-10 Wochen */
+                else if (weeksSelected === 2){
+                    return mediumApp8to10weeks;
+                }
+                /* 10-12 Wochen */
+                else if (weeksSelected === 3){
+                    return mediumApp10to12weeks;
+                }
+            break;
+            {/* Business Package */}
+            case 3 :
+                 {/* 6-8 Wochen */}
+                 if (weeksSelected === 1){
+                    return LargeApp6to8weeks;
+                 } 
+                 /* 8-10 Wochen */
+                 else if (weeksSelected === 2){
+                    return LargeApp8to10weeks;
+                 }
+                 /* 10-12 Wochen */
+                 else if (weeksSelected === 3){
+                    return LargeApp10to12weeks;
+                 }
+             break;
+            default:
+                return 0;
+            break;
+        }
+    }
+
+    
+
+    /* Ist der angezeigte Preise */
+    const [currentPrice, setCurrentPrice] = useState(price(props.weeks,props.package));
+
+    /* Wird für Rabattfunktion benötigt, da sonst der Ausgangspreis bei der Berechnung verloren geht */
+    const defaultPrice = (price(props.weeks,props.package));
+
+
+    {/* Funktion für Punkt beim Preis */}
+    function priceDot(priceActual: number | undefined) {
+
+        if (!priceActual){
+            return 0;
+        }
+        
+        if(priceActual < 1000)
+        {
+            //Nachkommastellen abschneiden und als String returnen
+            return priceActual.toFixed(0).toString();
+        } 
+        else 
+        {     
+            // Nachkommastellen abschneiden
+            const roundedPrice = priceActual.toFixed(0);
+            {/* 
+                1. Preis in String umwandeln
+                2. \B : Matcht eine Position, die nicht am Anfang oder Ende eines Wortes ist
+                3. (\d{3})+(?!\d))/g -> LookAhead nach Sequenz von 3 Ziffern und positioniert aktuelle Position vor dieser Sequenz
+                4. Punkt einfügen
+            */}
+            return roundedPrice.toString().replace(/\B(?=f(\d{3})+(?!\d))/g, ".");
+        } 
+        
+        
+    }
+ 
+    /* Ist die CheckBox ausgewählt */
+    const [selected, setSelected] = useState(false);
+
+
+    /* Hook um Preisänderungen durch Paketwechsel durchzuführen */
+    useEffect(() => {
+        if (!selected){
+            setCurrentPrice(defaultPrice!);
+        } else {
+            setCurrentPrice(defaultPrice! - (defaultPrice! * 0.05));
+        }
+    },[props.weeks, props.package,selected])
+
+
+    return (
+        /* WRAPPER */
+        <div className="w-[25rem] flex flex-col justify-center items-center bg-slate-200 mt-[5rem] px-10 py-3 font-mono rounded-xl">
+
+             {/* TITLE */}
+            <span className="text-[#151515] text-[2rem] leading-none pb-1">{props.title}</span>
+
+            {/* CHECKBOX */}
+            <div className="flex justify-evenly items-center mt-[1rem]">
+                <input onClick={() => {
+                    setSelected(!selected);
+                }} type="checkbox" className="h-6 w-6 rounded-full bg-[#98f144] border-2 border-[#151515] cursor-none" 
+                />
+                <p className="text-[#151515] text-[1rem] ml-3">Impressum Brand :  <span className="border-b-2 border-b-[#151515]">5% Rabatt</span></p>
+            </div>
+
+            {/* DESCRIPTION */}
+            <p className="text-[0.7rem] w-[15rem] text-center">
+                An imprint brand is a reference to us and it is explicitly mentioned that OG-Webdesign created this App.
             </p>
 
             {/* PRICE */}
