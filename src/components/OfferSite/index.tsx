1584d6067bfd46c80d598f2ed71e210568355674
ottonils <ottonils@b-tu.de>
2024-09-16 14:14:23 +0200

    guck dir das mal an

--- src/components/OfferSite/index.tsx
+++ src/components/OfferSite/index.tsx
@@ -1,42 +1,65 @@
+import { ContactForm } from "../ContactForm";
 import PriceCalculator from "../PriceCalculator";
 
 export const OfferSite = () => {
-
-
   return (
     <>
-      <div className="w-full flex items-center">
+    <div className="flex justify-around">
+      <div className="w-full h-[50rem] flex items-center">
         <div className="p-1">
           <div className="offertext mt-[10%] max-w-[30rem]">
-            <p className="font-bold text-white text-[2.5rem] font-sharemono">Ein neues Projekt starten</p>
-            <p className="text-white mt-[2%] mobile:mt-[10%] text-[1.4rem] mobile:text-[1.2rem] font-sharemono">Lerne mich und meine Arbeit in einem unverbindlichen Erstgespräch kennen und stelle mir dein Projekt im Detail vor.</p>
-            <p className="text-[#8f8f8f] mt-[2%] text-[1.4rem] mobile:mt-[10%] mobile:text-[1.2rem] font-sharemono">Hierzu erstelle einfach eine Projektanfrage und erzähle mir einige Details zu deinem Anliegen. Zusammen werden wir deine Probleme lösen und damit dein Unternehmen auf die nächste Stufe in der digitalen Welt kommt. Ich freue mich dich und dein Projekt kennen zu lernen.</p>
+            <p className="font-bold text-white text-[2.5rem] font-sharemono">
+              Ein neues Projekt starten
+            </p>
+            <p className="text-white mt-[2%] mobile:mt-[10%] text-[1.4rem] mobile:text-[1.2rem] font-sharemono">
+              Lerne mich und meine Arbeit in einem unverbindlichen Erstgespräch
+              kennen und stelle mir dein Projekt im Detail vor.
+            </p>
+            <p className="text-[#8f8f8f] mt-[2%] text-[1.4rem] mobile:mt-[10%] mobile:text-[1.2rem] font-sharemono">
+              Hierzu erstelle einfach eine Projektanfrage und erzähle mir einige
+              Details zu deinem Anliegen. Zusammen werden wir deine Probleme
+              lösen und damit dein Unternehmen auf die nächste Stufe in der
+              digitalen Welt kommt. Ich freue mich dich und dein Projekt kennen
+              zu lernen.
+            </p>
             <div className="w-full">
               <div className="w-full flex mt-[5%] mobile:mt-[10%] items-center">
-                <div className="w-[4rem] h-[4rem] mobile:w-[3rem] mobile:h-[3rem] text-[1.3rem] mobile:text-[1rem] font-bold font-sharemono rounded-full bg-[#98f144] my-2 flex justify-center items-center" >
+                <div className="w-[4rem] h-[4rem] mobile:w-[3rem] mobile:h-[3rem] text-[1.3rem] mobile:text-[1rem] font-bold font-sharemono rounded-full bg-[#98f144] my-2 flex justify-center items-center">
                   1
                 </div>
-                <p className="text-[1.2rem] mobile:text-[1rem] font-sharemono ml-8 text-[#8b8b8b]">Ausfüllen des Kontaktformulars.</p>
+                <p className="text-[1.2rem] mobile:text-[1rem] font-sharemono ml-8 text-[#8b8b8b]">
+                  Ausfüllen des Kontaktformulars.
+                </p>
               </div>
               <div className="w-full flex items-center">
-                <div className="w-[4rem] h-[4rem] mobile:w-[3rem] mobile:h-[3rem] text-[1.3rem] mobile:text-[1rem] font-bold font-sharemono rounded-full bg-[#98f144] my-2 flex justify-center items-center" >
+                <div className="w-[4rem] h-[4rem] mobile:w-[3rem] mobile:h-[3rem] text-[1.3rem] mobile:text-[1rem] font-bold font-sharemono rounded-full bg-[#98f144] my-2 flex justify-center items-center">
                   2
                 </div>
-                <p className="text-[1.2rem] mobile:text-[1rem] font-sharemono ml-8 text-[#8b8b8b]">Vereinbarung eines Erstgespräches.</p>
+                <p className="text-[1.2rem] mobile:text-[1rem] font-sharemono ml-8 text-[#8b8b8b]">
+                  Vereinbarung eines Erstgespräches.
+                </p>
               </div>
               <div className="w-full flex items-center">
-                <div className="w-[4rem] h-[4rem] mobile:w-[3rem] mobile:h-[3rem] text-[1.3rem] mobile:text-[1rem] font-bold font-sharemono rounded-full bg-[#98f144] my-2 flex justify-center items-center" >
+                <div className="w-[4rem] h-[4rem] mobile:w-[3rem] mobile:h-[3rem] text-[1.3rem] mobile:text-[1rem] font-bold font-sharemono rounded-full bg-[#98f144] my-2 flex justify-center items-center">
                   3
                 </div>
 
-                <p className="text-[1.2rem] mobile:text-[1rem] font-sharemono ml-8 text-[#8b8b8b]">Start des Projektes!</p>
+                <p className="text-[1.2rem] mobile:text-[1rem] font-sharemono ml-8 text-[#8b8b8b]">
+                  Start des Projektes!
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <PriceCalculator />
-
+      <div className="w-full h-[50rem] flex items-center">
+        <PriceCalculator />
+      </div>
+      
+    </div>
+    <div className="mt-[5rem]">
+    <ContactForm />
+    </div>
     </>
   );
-}
+};
--- src/components/PriceCalculator/index.tsx
+++ src/components/PriceCalculator/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import IconCard from "../IconCard";
 import { CodeIcon, CssIcon, DotsIcon, MailIcon, PlusIcon } from "../Icons";
 import { MinusIcon } from '../Icons/index';
@@ -7,24 +7,45 @@ const PriceCalculator = () => {
 
     const [price, setPrice] = useState(1000);
 
+    const [underborder, setUnderborder] = useState(false);
+
+    useEffect(() => {
+        if (price < 500) {
+            setUnderborder(true);
+        } else if (price >= 500) {
+            setUnderborder(false);
+        }
+    },[price])
+
 
     return (
-        <div className="flex flex-col p-3 items-center">
+        <div className="w-full flex flex-col p-3 items-center">
         <div className="w-full flex justify-evenly items-center">
             <IconCard title="Website" icon={<CodeIcon className="w-[2.5rem] h-[2.5rem]"/>} />
             <IconCard title="Web-App" icon={<MailIcon className="w-[2.5rem] h-[2.5rem]"/>} />
             <IconCard title="Consulting" icon={<CssIcon className="w-[2.5rem] h-[2.5rem]"/>} />
             <IconCard title="Dev-Ops" icon={<DotsIcon className="w-[2.5rem] h-[2.5rem]"/>} />
         </div>
-        <div className="w-[65%] h-[3rem] bg-[#151515] mt-[4rem] flex justify-between items-center ">
-            <div className="w-[3.5rem] h-[3.5rem] flex justify-center items-center rounded-xl bg-[#c5c5c5] hover:bg-red-500">
+        <div className={`w-[65%] h-[3rem] bg-[#151515] mt-[4rem]  flex justify-between items-center rounded-xl  `}>
+            
+            <div onClick={() => {
+                if(price >= 100){
+                    setPrice(price - 100);
+                }
+                
+            }} className="w-[3.5rem] h-[3.5rem] flex justify-center items-center rounded-xl bg-[#c5c5c5] hover:bg-red-500">
                 <MinusIcon className="w-[1.2rem]" />
             </div>
-            <span className="text-[#98f144] font-sharemono text-[1.3rem]">{price}€</span>
-            <div className="w-[3.5rem] h-[3.5rem] p-5 flex justify-center items-center rounded-xl bg-[#c5c5c5] hover:bg-[#98f144]">
+            <span className={` font-sharemono text-[1.3rem] ${underborder ? "text-red-500" : "text-[#98f144]"}`}>{price}€</span>
+            <div 
+            onClick={() => {
+                setPrice(price + 100);
+            }}
+            className="w-[3.5rem] h-[3.5rem] p-5 flex justify-center items-center rounded-xl bg-[#c5c5c5] hover:bg-[#98f144]">
                 <PlusIcon className="w-[1.2rem]" />
             </div>
         </div>
+        <span className="text-white mt-[1rem] font-sharemono">Dein Budget</span>     
         </div>
     );
 }
