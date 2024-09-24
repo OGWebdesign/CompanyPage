586bb5422d8928e2592d3b173a58fe467253f1ab
BummfangTV <maximiliangornw@web.de>
2024-09-24 13:10:40 +0200

    wip

--- src/components/StartSite/index.tsx
+++ src/components/StartSite/index.tsx
@@ -24,19 +24,14 @@ export const StartSite = (props: {
           text="Web development at an academic level and the latest web and application technologies give your company unimaginable power in its web presence or internal company processes."
           reference="/contact"
         />
-        <ParagraphOG classnamehead="text-slate-200 mt-[10%]" head="Compiled with the same technologies as Facebook and Instagram. "
-          text="Using the latest front-end and back-end technologies, we achieve loading speeds that no content management system can even remotely match. 
-                Loading times as low as 300 milliseconds.
-                We make no compromises when it comes to design. Modern, timeless designs where there are no limits to the possibilities. 
-                We do not use any modular systems and for this reason we are not subject to any restrictions in the entire development process."
-          animation={true} classname="text-right justify-end"></ParagraphOG>
+
+        <TextComponent
+          headline="High-speed performance for websites and web applications on the next level."
+          text="We have consciously decided against content management solutions when developing websites. On the one hand, we have set ourselves the task of revolutionizing the Internet with highly customized customer projects. We want to help companies that have understood how to stand out from the competition to offer an alternative to offers that mainly consist of content management systems. On the other hand, full control over the project is important. It is particularly important for perfectionist developers like us to be able to change even the finer details. Performance and maintainability are particularly guaranteed by the technologies we use. We can log all development steps of a project and reset them if necessary. Nothing unexpected happens here! We make no compromises when it comes to performance. The code base, the media, and even the fonts are professionally compressed so that quality losses are not visible. All packed together in a state-of-the-art and reliable ecosystem with the highest level of care for maintainability. 80% of a software product consists of maintenance and care."
+        ></TextComponent>
       </div>
 
 
-      <TextComponent
-        headline="High-speed performance for websites and web applications on the next level."
-        text="We have consciously decided against content management solutions when developing websites. On the one hand, we have set ourselves the task of revolutionizing the Internet with highly customized customer projects. We want to help companies that have understood how to stand out from the competition to offer an alternative to offers that mainly consist of content management systems. On the other hand, full control over the project is important. It is particularly important for perfectionist developers like us to be able to change even the finer details. Performance and maintainability are particularly guaranteed by the technologies we use. We can log all development steps of a project and reset them if necessary. Nothing unexpected happens here! We make no compromises when it comes to performance. The code base, the media, and even the fonts are professionally compressed so that quality losses are not visible. All packed together in a state-of-the-art and reliable ecosystem with the highest level of care for maintainability. 80% of a software product consists of maintenance and care."
-      ></TextComponent>
 
       {/* WrapperLayout */}
       <div className="w-full flex justify-center items-center mt-[10rem]">
--- src/components/TextComponent/index.tsx
+++ src/components/TextComponent/index.tsx
@@ -3,7 +3,7 @@ export const TextComponent = (props:{
     text:string
 }) => {
     return (
-        <div className="w-full mt-[3rem] flex flex-col justify-center items-center">
+        <div className="w-full mt-[10%] flex flex-col justify-center items-center">
             <p className="text-[#98f144] w-[80%] leading-9 text-[2rem] font-mono font-semibold">{props.headline}</p>
             <p className="text-slate-200 w-[80%] leading-6 mt-5  font-mono">{props.text}</p>
         </div>
