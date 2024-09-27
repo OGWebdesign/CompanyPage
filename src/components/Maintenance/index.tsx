4ef380209df68f556efad3dccd55d872225cdfec
ottonils <ottonils@b-tu.de>
2024-09-27 22:05:57 +0200

    fix

--- src/components/Maintenance/index.tsx
+++ src/components/Maintenance/index.tsx
@@ -4,7 +4,7 @@ export const Maintenance = (props: {
     children?: JSX.Element | JSX.Element[] | string,
 }) => {
     return (
-        <div className="w-full animate-fadeIn cursor-none flex items-center  justify-center flex-col h-full fixed z-10 bg-[#151515]">
+        <div className="w-full h-screen animate-fadeIn cursor-none flex items-center  justify-center flex-col z-10 bg-black">
             <div className="flex flex-col mobile:p-10">
                 <p className="text-white font-mono text-[2rem] inline-block bg-[#]">Maintenance work in progress in this area</p>
                 <p className="text-white w-[25rem] font-mono text-[1rem]">This area of ​​the site is undergoing maintenance and will therefore be unavailable for a while.</p>
