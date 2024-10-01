8f98f4ef288e8cd50493821dabf147003272a7b3
BummfangTV <maximiliangornbw@web.de>
2024-10-01 14:16:06 +0200

    init component prototype, no functionality

--- src/components/RequestContent/index.tsx
+++ src/components/RequestContent/index.tsx
@@ -2,12 +2,54 @@
 
 export const RequestContent = () => {
     return (
-        <div className="w-full flex-col h-[40rem] animate-zeptaEffect overflow-hidden text-white font-sharemono text-[2rem] font-bold border rounded-2xl flex items-center ">
-            <div className="w-full h-[4rem] flex items-center border">
-                <p className="ml-8 text-[1.4rem]">Ich habe feste Vorstellungen von Budget und Projekt.</p>
+        <div className="w-full flex-col h-[32rem] animate-zeptaEffect overflow-hidden text-white font-sharemono text-[2rem] font-bold rounded-2xl flex items-center ">
+            <div className="w-full h-[4rem] flex items-center justify-between">
+                <p className="ml-8 text-[1.4rem]">Wie kann ich Kontakt zu euch aufnehmen?</p>
+                <div className="w-[10rem] h-[3rem] rounded-2xl bg-[#88ff27] flex justify-center items-center">
+                    <p className="text-[1.2rem] text-black">Antwort</p>
+                </div>
             </div>
-            <div className="w-full h-[4rem] flex items-center border">
-                <p className="ml-8 text-[1.4rem]">Ich möchte mich erst einmal über die Möglichkeiten beraten lassen.</p>
+            <div className="w-full h-[4rem] flex items-center justify-between">
+                <p className="ml-8 text-[1.4rem]">Warum wird die Seite nicht korrekt geladen oder angezeigt?</p>
+                <div className="w-[10rem] h-[3rem] rounded-2xl bg-[#88ff27] flex justify-center items-center">
+                    <p className="text-[1.2rem] text-black">Antwort</p>
+                </div>
+            </div>
+            <div className="w-full h-[4rem] flex items-center justify-between">
+                <p className="ml-8 text-[1.4rem]">Wie kann ich euch ein Angebot machen?</p>
+                <div className="w-[10rem] h-[3rem] rounded-2xl bg-[#88ff27] flex justify-center items-center">
+                    <p className="text-[1.2rem] text-black">Antwort</p>
+                </div>
+            </div>
+            <div className="w-full h-[4rem] flex items-center justify-between">
+                <p className="ml-8 text-[1.4rem]">Wie kann ich euch bei allgemeinen Fragen kontaktieren?</p>
+                <div className="w-[10rem] h-[3rem] rounded-2xl bg-[#88ff27] flex justify-center items-center">
+                    <p className="text-[1.2rem] text-black">Antwort</p>
+                </div>
+            </div>
+            <div className="w-full h-[4rem] flex items-center justify-between">
+                <p className="ml-8 text-[1.4rem]">Kann ich mich von euch über eure Dienstleistungen beraten lassen?</p>
+                <div className="w-[10rem] h-[3rem] rounded-2xl bg-[#88ff27] flex justify-center items-center">
+                    <p className="text-[1.2rem] text-black">Antwort</p>
+                </div>
+            </div>
+            <div className="w-full h-[4rem] flex items-center justify-between">
+                <p className="ml-8 text-[1.4rem]">Wann sind eure Sprechzeiten?</p>
+                <div className="w-[10rem] h-[3rem] rounded-2xl bg-[#88ff27] flex justify-center items-center">
+                    <p className="text-[1.2rem] text-black">Antwort</p>
+                </div>
+            </div>
+            <div className="w-full h-[4rem] flex items-center justify-between">
+                <p className="ml-8 text-[1.4rem]">Welche Technologien nutzt ihr zur Entwicklung?</p>
+                <div className="w-[10rem] h-[3rem] rounded-2xl bg-[#88ff27] flex justify-center items-center">
+                    <p className="text-[1.2rem] text-black">Antwort</p>
+                </div>
+            </div>
+            <div className="w-full h-[4rem] flex items-center justify-between">
+                <p className="ml-8 text-[1.4rem]">Wie lange dauert der Entwicklungsprozess?</p>
+                <div className="w-[10rem] h-[3rem] rounded-2xl bg-[#88ff27] flex justify-center items-center">
+                    <p className="text-[1.2rem] text-black">Antwort</p>
+                </div>
             </div>
         </div>
     );
