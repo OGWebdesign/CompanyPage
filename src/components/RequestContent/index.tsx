b52b54a30bacf859bd3ae64cdd9368ec6343f758
BummfangTV <maximiliangornbw@web.de>
2024-10-01 13:36:06 +0200

    fix

--- src/components/RequestContent/index.tsx
+++ src/components/RequestContent/index.tsx
@@ -1,8 +1,14 @@
+
+
 export const RequestContent = () => {
     return (
-        <div className="w-full h-[40rem] animate-zeptaEffect
-         overflow-hidden text-white font-sharemono text-[2rem] font-bold border rounded-2xl flex justify-center items-center">
-            
+        <div className="w-full flex-col h-[40rem] animate-zeptaEffect overflow-hidden text-white font-sharemono text-[2rem] font-bold border rounded-2xl flex items-center ">
+            <div className="w-full h-[4rem] flex items-center border">
+                <p className="ml-8 text-[1.4rem]">Ich habe feste Vorstellungen von Budget und Projekt.</p>
+            </div>
+            <div className="w-full h-[4rem] flex items-center border">
+                <p className="ml-8 text-[1.4rem]">Ich möchte mich erst einmal über die Möglichkeiten beraten lassen.</p>
+            </div>
         </div>
     );
 }
